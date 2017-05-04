4eb3fadabf8d0cc4f5acf3dd09583448f7adfdf1
MadmanChen <madmanchen@hsinten.com.tw>
2017-05-04 09:29:06 +0800

    Fix VersionChecker

--- sample/src/main/java/tw/idv/madmanchen/mdutils/MainActivity.java
+++ sample/src/main/java/tw/idv/madmanchen/mdutils/MainActivity.java
@@ -6,13 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import java.util.HashMap;
-
-import tw.idv.JSONException;
-import tw.idv.JSONObject;
-import tw.idv.madmanchen.mdhttpasynctasklib.MDHttpAsyncTask;
-import tw.idv.madmanchen.mdutilslib.base.VersionChecker;
-
 public class MainActivity extends Activity {
     Context mContext;
 
@@ -22,46 +15,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Log.e("onCreate", "onCreate");
         mContext = this;
-        new VersionChecker.Builder()
-                .setServerUrl("http://www.xingmerit.com.cn/appupdate/")
-                .setCheckType(VersionChecker.SERVER)
-                .addServerData("pkgName", "com.agenttw")
-                .addServerData("verCode", "27")
-                .setUpdateView(mContext, "", "", "up", false)
-                .build()
-                .check(new VersionChecker.SubCheck() {
-                    @Override
-                    public void onChecked(Object result) {
-                        Log.e("versionChecker", result.toString());
-                    }
-                });
-
-        HashMap<String, String> postData = new HashMap<>();
-        postData.put("acc", "MADMANCHEN");
-        postData.put("psd", "Codeing");
-        postData.put("page", "branchsale");
-        postData.put("act", "branchsale_getdata");
-
-       new MDHttpAsyncTask.Builder()
-                .load("http://pub.mysoqi.com/ht_analy/0028/")
-                .addPostData(postData)
-                .setLoadingView(mContext, "", "loading")
-                .build()
-                .startAll(new MDHttpAsyncTask.SubResponse() {
-                    @Override
-                    public void onResponse(Object o) {
-                        if (o != null) {
-                            try {
-                                JSONObject jsonObject = new JSONObject(o.toString());
-                                boolean result = jsonObject.optBoolean("result");
-                                String msg = jsonObject.optString("msg");
-                                Log.e("json", jsonObject.toString());
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                });
     }
 
 

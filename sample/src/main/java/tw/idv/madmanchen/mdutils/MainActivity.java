add6c19684ef75a816ac70ccefbf4b5b07377378
MadmanChen <madmanchen@hsinten.com.tw>
2017-05-04 15:23:09 +0800

    Update MDHttpTask to 1.0.2

--- sample/src/main/java/tw/idv/madmanchen/mdutils/MainActivity.java
+++ sample/src/main/java/tw/idv/madmanchen/mdutils/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import tw.idv.madmanchen.mdutilslib.base.BaseActivity;
-import tw.idv.madmanchen.mdutilslib.base.VersionChecker;
 
 public class MainActivity extends BaseActivity {
     Context mContext;
@@ -17,19 +16,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         Log.e("onCreate", "onCreate");
         mContext = this;
-        new VersionChecker.Builder()
-                .setCheckType(VersionChecker.SERVER)
-                .setServerUrl("http://www.xingmerit.com.cn/appupdate/")
-                .setUpdateView(mContext, "", "Info", "Update", false)
-                .addServerData("pkgName", "com.hsinmerit")
-                .addServerData("verCode", "9")
-                .build()
-                .check(new VersionChecker.SubCheck() {
-                    @Override
-                    public void onChecked(Object o) {
 
-                    }
-                });
 
     }
 
